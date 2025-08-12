b11dd8f327dc70dc38bf80abc5a2dd994edacb5f
harshini202005 <harshupranav203@gmail.com>
2025-08-12 11:51:28 +0530

    File added

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request
 import pickle
 import pandas as pd
-
+import os
 app = Flask(__name__)
 
-# Load model
+
 with open('attrition_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
-# Features list
+
 features = ['ID', 'Age', 'DistanceFromHome', 'MonthlyIncome',
             'JobSatisfaction', 'YearsAtCompany', 'WorkLifeBalance',
             'Education', 'Overtime']
@@ -20,7 +20,7 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Get form data
+
         input_data = [
             int(request.form['ID']),
             int(request.form['Age']),
@@ -33,10 +33,10 @@ def predict():
             int(request.form['Overtime'])
         ]
 
-        # Create DataFrame for prediction
+       
         df_input = pd.DataFrame([input_data], columns=features)
         
-        # Predict
+      
         prediction = model.predict(df_input)[0]
         result_text = "Employee is likely to leave the company." if prediction == 1 else "Employee is likely to stay."
 
@@ -46,4 +46,5 @@ def predict():
         return f"Error: {str(e)}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+       port = int(os.environ.get("PORT", 10000))
+       app.run(host='0.0.0.0', port=port)
